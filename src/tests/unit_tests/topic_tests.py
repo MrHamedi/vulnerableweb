c26e038488b636477e97fd341ea86b68d79dace4
MrHamedi <ahmadihamed167@gmail.com>
2023-08-04 21:56:57 +0330

    TopicTest created

---
+++ src/tests/unit_tests/topic_tests.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from topic.models import Topic
+
+
+class TopicTest(TestCase):
+
+    def setUp(self)->None:
+        self.topic_1=Topic.objects.create(title="sqli" ,text="sqli is an "
+                                          "attack in which the hacker ...")
+        self.topic_2=Topic.objects.create(title="xss" ,text="xss is an "
+                                          "attack in which the hacker ...")
+        
+    def test_topic_can_get_created_and_found(self)->None:
+        self.assertEqual(Topic.objects.count() ,2 ,"all the topic objects "
+                         "did not get created!")
+        
+        self.assertEqual(self.topic_2.title ,"sqli" ,"The title of topic "
+                         "was not set correctly!")
+        
+        self.assertEqual(self.topic_1.text ,"xss is an attack in which the "
+                         "hacker ..." ,"The text of topic object was not set "
+                         "correctly!")
+        
+
