0a5986712f6aac48acc15496fd2d97f7e1dc94e5
MrHamedi <ahmadihamed167@gmail.com>
2023-08-10 16:33:53 +0330

    new tests added for checking the content of topic detail page

--- src/tests/unit_tests/topic_tests.py
+++ src/tests/unit_tests/topic_tests.py
@@ -5,7 +5,7 @@ from topics.models import Topic
 class TopicTest(TestCase):
 
     def setUp(self)->None:
-        self.topic_1=Topic.objects.create(title="sqli" ,text="sqli is an "
+        self.topic_1=Topic.objects.create(title="Sql injection" ,text="sqli is an "
                                           "attack in which the hacker ...")
         self.topic_2=Topic.objects.create(title="xss" ,text="xss is an "
                                           "attack in which the hacker ...")
@@ -20,3 +20,16 @@ class TopicTest(TestCase):
         self.assertEqual(self.topic_2.text ,"xss is an attack in which the "
                          "hacker ..." ,"The text of topic object was not set "
                          "correctly!")
+
+    def test_land_detail_page(self):
+        uuid=self.topic_1.uuid
+        response=self.client.get('/topics/'+str(uuid)+"/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "topics/topic_detail.html")
+
+    def test_detail_page_contains(self):
+        uuid=self.topic_1.uuid
+        response=self.client.get('/topics/'+str(uuid)+"/")
+
+        self.assertContains(response, "Sql injection")
+        self.assertContains(response, "sqli is an attack in which the hacker ...")  
