3ad45daba93e0e91113a0d83909eb18fd3c14c50
MrHamedi <ahmadihamed167@gmail.com>
2023-08-10 18:36:27 +0330

    test_detail_page_no_image  test  added

--- src/tests/unit_tests/topic_tests.py
+++ src/tests/unit_tests/topic_tests.py
@@ -1,14 +1,28 @@
+import os 
+
 from django.test import TestCase
+
 from topics.models import Topic
 
 
 class TopicTest(TestCase):
 
     def setUp(self)->None:
-        self.topic_1=Topic.objects.create(title="Sql injection" ,text="sqli is an "
-                                          "attack in which the hacker ...")
-        self.topic_2=Topic.objects.create(title="xss" ,text="xss is an "
-                                          "attack in which the hacker ...")
+        # Get the absolute path of the current directory
+        current_directory = os.path.abspath(os.path.dirname(__file__))
+
+        self.image_path = os.path.join(current_directory, '..', \
+                                       'test_resources', 'dummy.png')
+        self.topic_1=Topic.objects.create(title="Sql injection",
+                                          text="sqli is an attack in "
+                                            "which the hacker...",
+                                          pic=self.image_path
+                                          )
+        self.topic_2=Topic.objects.create(title="xss",
+                                          text="xss is an attack in which "
+                                          "the hacker ...",
+                                          pic=self.image_path
+                                          )
         
     def test_topic_can_get_created_and_found(self)->None:
         self.assertEqual(Topic.objects.count() ,2 ,"all the topic objects "
@@ -32,4 +46,11 @@ class TopicTest(TestCase):
         response=self.client.get('/topics/'+str(uuid)+"/")
 
         self.assertContains(response, "Sql injection")
-        self.assertContains(response, "sqli is an attack in which the hacker ...")  
+        self.assertContains(response, "sqli is an attack in which the "
+                            "hacker...")  
+        
+    def test_detail_page_no_image(self):
+        self.topic_1.pic=None
+        uuid=self.topic_1.uuid
+        response=self.client.get('/topics/'+str(uuid)+"/")
+        self.assertEqual(response.status_code, 200)
