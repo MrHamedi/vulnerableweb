f4013f0a1bca9c25a33effb512dc42b2abc44cba
MrHamedi <ahmadihamed167@gmail.com>
2023-09-06 23:47:39 +0330

    Exercise model added to admin

--- src/exercise/admin.py
+++ src/exercise/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Exercise, ExerciseTip
 
-# Register your models here.
+class ExerciseTipInline(admin.TabularInline):
+    model = ExerciseTip
+
+class ExerciseAdmin(admin.ModelAdmin):
+    ordering = ['created', 'updated']
+    search_fields = ["topic", "title"]
+    list_display = ["title", "topic", 'created', 'updated']
+    inlines = [
+        ExerciseTipInline
+    ]
+
+admin.site.register(Exercise, ExerciseAdmin)
