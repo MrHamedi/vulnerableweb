65c4e335eda548b7339d5833f504bb4edbbe388c
MrHamedi <ahmadihamed167@gmail.com>
2023-09-08 19:30:39 +0330

    Password hasher replaced with one that don't add salt

--- src/vulnerableweb/settings/base.py
+++ src/vulnerableweb/settings/base.py
@@ -135,3 +135,8 @@ MEDIA_URL="/media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PASSWORD_HASHERS = [
+    #replacing the django django password salter with no salt hasher.
+    'core.utils.NoSaltPasswordHasher',
+]
